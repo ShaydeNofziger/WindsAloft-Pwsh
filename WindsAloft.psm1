8c60f220adecc7855618b3f9455cc347a9d1e58d
Shayde Nofziger <snofziger@microsoft.com>
2022-12-07 12:58:32 -0500

    Update readme, add initial module

---
+++ WindsAloft.psm1
@@ -0,0 +1,66 @@
+$Global:WindsAloftDropZoneList = $null
+
+function Open-WindsAloft {
+    [CmdletBinding()]
+    param(
+        [Parameter (Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DropZoneName
+        )
+    $UrlTemplate = 'https://windsaloft.us/?lat={LAT}&lon={LON}'
+
+    $DropZoneList = Get-WindsAloftDropZoneList
+
+    $DropZone = $DropZoneList | Where-Object { $_.DropZoneName -eq $DropZoneName }
+
+    $Url = $UrlTemplate -replace '{LAT}', $DropZone.DropZoneLat -replace '{LON}', $DropZone.DropZoneLong
+
+    Start-Process -FilePath $Url
+}
+
+function Get-WindsAloftDropZoneList {
+    if ($null -eq $Global:WindsAloftDropZoneList) {
+        $result = Invoke-RestMethod -Method Get -Uri 'https://windsaloft.us/dropzones.geojson'
+    
+        $Global:WindsAloftDropZoneList = $result.features | ForEach-Object {
+            [PSCustomObject]@{
+                DropZoneName = $_.properties.Name
+                DropZoneLongitude = $_.geometry.coordinates[0]
+                DropZoneLatitude = $_.geometry.coordinates[1]
+            }
+        }
+    }
+
+    Write-Output $Global:WindsAloftDropZoneList
+
+}
+
+function Get-WindsAloftDropZone {
+    [CmdletBinding()]
+    param(
+        [Parameter (Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DropZoneName
+    )
+
+    Get-WindsAloftDropZoneList | Where-Object -Property 'DropZoneName' -eq $DropZoneName
+}
+
+function Get-WindsAloftData {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DropZoneLatitude,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DropZoneLongitude
+    )
+
+    $Referer = 'SHAYDE'
+
+    $Url = "https://windsaloft.us/winds.php?lat=$DropZoneLatitude&lon=$DropZoneLongitude&hourOffset=0&referrer=$Referer"
+
+    Invoke-RestMethod -Method Get -Uri $Url
+}
